88c99e3cc022f1ba260ee6232e1fb9d7dbab2306
Supachai.s <supachai.s@pointpower.co.th>
2019-09-14 17:04:30 +0700

    int to string convert

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+)
+
+var (
+	i int     = 100
+	j float64 = 200.50
 )
 
 func main() {
-	fmt.Print("Hello World")
+	fmt.Printf("%v, %T \r\n", i, i)
+
+	k := strconv.Itoa(i)
+	fmt.Printf("%v, %T", k, k)
 }
